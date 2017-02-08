32876929bd3ed7b669130bf2f60cfd70602532fe
accurateness <GhostfishDev11@outlook.com>
2017-02-08 12:40:31 +0700

    First Commit

---
+++ components/Calendar.js
@@ -0,0 +1,354 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  Dimensions,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  Image
+} from 'react-native';
+
+import Day from './Day';
+
+import moment from 'moment';
+import styles from './styles';
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const VIEW_INDEX = 2;
+
+export default class Calendar extends Component {
+
+  state = {
+    currentMonthMoment: moment(this.props.startDate),
+    selectedMoment: moment(this.props.selectedDate),
+  };
+
+  static propTypes = {
+    customStyle: PropTypes.object,
+    dayHeadings: PropTypes.array,
+    eventDates: PropTypes.array,
+    monthNames: PropTypes.array,
+    nextButtonText: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object
+    ]),
+    onDateSelect: PropTypes.func,
+    onSwipeNext: PropTypes.func,
+    onSwipePrev: PropTypes.func,
+    onTouchNext: PropTypes.func,
+    onTouchPrev: PropTypes.func,
+    prevButtonText: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object
+    ]),
+    scrollEnabled: PropTypes.bool,
+    selectedDate: PropTypes.any,
+    showControls: PropTypes.bool,
+    showEventIndicators: PropTypes.bool,
+    startDate: PropTypes.any,
+    titleFormat: PropTypes.string,
+    today: PropTypes.any,
+    weekStart: PropTypes.number,
+  };
+
+  static defaultProps = {
+    customStyle: {},
+    width: DEVICE_WIDTH,
+    dayHeadings: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    eventDates: [],
+    monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    nextButtonText: 'Next',
+    prevButtonText: 'Prev',
+    scrollEnabled: false,
+    showControls: false,
+    showEventIndicators: false,
+    startDate: moment().format('YYYY-MM-DD'),
+    titleFormat: 'MMMM YYYY',
+    today: moment(),
+    weekStart: 1,
+  };
+
+  componentDidMount() {
+    // fixes initial scrolling bug on Android
+    setTimeout(() => this.scrollToItem(VIEW_INDEX), 0)
+  }
+
+  componentDidUpdate() {
+    this.scrollToItem(VIEW_INDEX);
+  }
+  
+  componentWillReceiveProps(props) {
+    if (props.selectedDate) {
+      this.setState({selectedMoment: props.selectedDate});
+    }
+  }
+
+  getMonthStack(currentMonth) {
+    if (this.props.scrollEnabled) {
+      const res = [];
+      for (let i = -VIEW_INDEX; i <= VIEW_INDEX; i++) {
+        res.push(moment(currentMonth).add(i, 'month'));
+      }
+      return res;
+    }
+    return [moment(currentMonth)];
+  }
+
+ prepareEventDates( events) {
+    const parsedDates = {};
+
+
+    // Dates with custom properties
+    if (events) {
+      events.forEach(event => {
+        if (event.date) {
+            const date = moment(event.date);
+            const month = moment(date).startOf('month').format();
+            parsedDates[month] = parsedDates[month] || {};
+            parsedDates[month][date.date() - 1] = event;
+        }
+      });
+    }
+    return parsedDates;
+  }
+
+  prepareInActiveDates( inActiveDays) {
+    const parsedDates = {};
+    // Dates with custom properties
+    if (inActiveDays) {
+      inActiveDays.forEach(event => {
+        if (event.date) {
+            const date = moment(event.date);
+            const month = moment(date).startOf('month').format();
+            parsedDates[month] = parsedDates[month] || {};
+            parsedDates[month][date.date() - 1] = event;
+        }
+      });
+    }
+    return parsedDates;
+  }
+
+  selectDate(date) {
+    this.setState({ selectedMoment: date });
+    this.props.onDateSelect && this.props.onDateSelect(date ? date.format(): null );
+  }
+
+  onPrev = () => {
+    const newMoment = moment(this.state.currentMonthMoment).subtract(1, 'month');
+    this.setState({ currentMonthMoment: newMoment });
+    this.props.onTouchPrev && this.props.onTouchPrev(newMoment);
+  }
+
+  onNext = () => {
+    const newMoment = moment(this.state.currentMonthMoment).add(1, 'month');
+    this.setState({ currentMonthMoment: newMoment });
+    this.props.onTouchNext && this.props.onTouchNext(newMoment);
+  }
+
+  scrollToItem(itemIndex) {
+    const scrollToX = itemIndex * this.props.width;
+    if (this.props.scrollEnabled) {
+      this._calendar.scrollTo({ y: 0, x: scrollToX, animated: false });
+    }
+  }
+
+  scrollEnded(event) {
+    const position = event.nativeEvent.contentOffset.x;
+    const currentPage = position / this.props.width;
+    const newMoment = moment(this.state.currentMonthMoment).add(currentPage - VIEW_INDEX, 'month');
+    this.setState({ currentMonthMoment: newMoment });
+
+    if (currentPage < VIEW_INDEX) {
+      this.props.onSwipePrev && this.props.onSwipePrev(newMoment);
+    } else if (currentPage > VIEW_INDEX) {
+      this.props.onSwipeNext && this.props.onSwipeNext(newMoment);
+    }
+  }
+
+  renderMonthView(argMoment, eventsMap, inActiveDatesMap) {
+    const { dayInActive } = this.props
+    let
+      renderIndex = 0,
+      weekRows = [],
+      days = [],
+      startOfArgMonthMoment = argMoment.startOf('month');
+
+    const
+      selectedMoment = moment(this.state.selectedMoment),
+      weekStart = this.props.weekStart,
+      todayMoment = moment(this.props.today),
+      todayIndex = todayMoment.date() - 1,
+      argMonthDaysCount = argMoment.daysInMonth(),
+      offset = (startOfArgMonthMoment.isoWeekday() - weekStart + 7) % 7,
+      argMonthIsToday = argMoment.isSame(todayMoment, 'month'),
+      selectedIndex = moment(selectedMoment).date() - 1,
+      selectedMonthIsArg = selectedMoment.isSame(argMoment, 'month');
+
+    const events = (eventsMap !== null)
+      ? eventsMap[argMoment.startOf('month').format()]
+      : null;
+
+    const inActive = (inActiveDatesMap !== null)
+    ? inActiveDatesMap[argMoment.startOf('month').format()]
+    : null;
+
+    do {
+      const dayIndex = renderIndex - offset;
+      const isoWeekday = (renderIndex + weekStart) % 7;
+
+      if (dayIndex >= 0 && dayIndex < argMonthDaysCount) {
+        days.push((
+          <Day
+            startOfMonth={startOfArgMonthMoment}
+            key={`${renderIndex}`}
+            onPress={() => {
+              this.selectDate(moment(startOfArgMonthMoment).set('date', dayIndex + 1));
+            }}
+            caption={`${dayIndex + 1}`}
+            isToday={argMonthIsToday && (dayIndex === todayIndex)}
+            isSelected={selectedMonthIsArg && (dayIndex === selectedIndex)}
+            event={events && events[dayIndex]}
+            inActive={inActive && inActive[dayIndex]}
+            showEventIndicators={this.props.showEventIndicators}
+            customStyle={this.props.customStyle}
+          />
+        ));
+      } else {
+        days.push(<Day key={`${renderIndex}`} filler customStyle={this.props.customStyle} />);
+      }
+      if (renderIndex % 7 === 6) {
+        weekRows.push(
+          <View
+            key={weekRows.length}
+            style={[styles.weekRow, this.props.customStyle.weekRow]}
+          >
+            {days}
+          </View>);
+        days = [];
+        if (dayIndex + 1 >= argMonthDaysCount) {
+          break;
+        }
+      }
+      renderIndex += 1;
+    } while (true)
+    const containerStyle = [styles.monthContainer, this.props.customStyle.monthContainer];
+    return <View key={argMoment.month()} style={containerStyle}>{weekRows}</View>;
+  }
+
+  renderHeading() {
+    const headings = [];
+    for (let i = 0; i < 7; i++) {
+      const j = (i + this.props.weekStart) % 7;
+      headings.push(
+        <Text
+          key={i}
+          style={[styles.dayHeading, this.props.customStyle.dayHeading]}
+        >
+          {this.props.dayHeadings[j]}
+        </Text>
+      );
+    }
+
+    return (
+      <View style={[styles.calendarHeading, this.props.customStyle.calendarHeading]}>
+        {headings}
+      </View>
+    );
+  }
+  // <Text style={[styles.controlButtonText, this.props.customStyle.controlButtonText]}>
+  //             {this.props.prevButtonText}
+  //           </Text>
+
+  // <Text style={[styles.controlButtonText, this.props.customStyle.controlButtonText]}>
+  //             {this.props.nextButtonText}
+  //           </Text>
+
+  renderLoanDate(){
+    const { fromDate, toDate } = this.props
+    return(
+      <View style={styles.viewLoanDate}>
+          <View style={styles.viewLoanFromDate}>
+            <Text style={styles.txtLoanDate}>{fromDate.date}</Text>
+            <Text style={styles.txtLoanDate}>{fromDate.day}</Text>
+          </View>
+          <View style={styles.viewLoanCenterDate}>
+            <Image source={require('../img/slash.png')} style={{width:50,height:50}} />
+          </View>
+          <View style={styles.viewLoanToDate}>
+            <Text style={styles.txtLoanDate}>{toDate.date}</Text>
+            <Text style={styles.txtLoanDate}>{toDate.day}</Text>
+          </View>
+      </View> 
+    )
+  }
+
+  renderTopBar() {
+    let localizedMonth = this.props.monthNames[this.state.currentMonthMoment.month()];
+    localizedMonth = localizedMonth.toUpperCase();
+    return this.props.showControls
+    ? (
+        <View style={[styles.calendarControls, this.props.customStyle.calendarControls]}>
+          <TouchableOpacity
+            style={[styles.controlLeftButton, this.props.customStyle.controlButton]}
+            onPress={this.onPrev}
+          >
+            <Image source={require('../img/prev.png')} style={styles.icon} />
+            
+          </TouchableOpacity>
+          <View style={styles.headerCenter}>
+            <Text style={[styles.title, this.props.customStyle.title]}>
+              {localizedMonth} {this.state.currentMonthMoment.year()}
+            </Text>
+          </View>
+          <TouchableOpacity
+            style={[styles.controlRightButton, this.props.customStyle.controlButton]}
+            onPress={this.onNext}
+          >
+            
+            <Image source={require('../img/next.png')} style={styles.icon} />
+          </TouchableOpacity>
+        </View>
+      )
+    : (
+      <View style={[styles.calendarControls, this.props.customStyle.calendarControls]}>
+        <Text style={[styles.title, this.props.customStyle.title]}>
+          {this.state.currentMonthMoment.format(this.props.titleFormat)}
+        </Text>
+      </View>
+    );
+  }
+
+  render() {
+    const calendarDates = this.getMonthStack(this.state.currentMonthMoment);
+    const eventDatesMap = this.prepareEventDates(this.props.events);
+    const inActiveDatesMap = this.prepareInActiveDates(this.props.dayInActive);
+
+    return (
+      <View style={[styles.calendarContainer, this.props.customStyle.calendarContainer]}>
+        {this.renderTopBar()}
+        {this.renderLoanDate()}
+        {this.renderHeading(this.props.titleFormat)}
+        {this.props.scrollEnabled ?
+          <ScrollView
+            ref={calendar => this._calendar = calendar}
+            horizontal
+            scrollEnabled
+            pagingEnabled
+            removeClippedSubviews
+            scrollEventThrottle={1000}
+            showsHorizontalScrollIndicator={false}
+            automaticallyAdjustContentInsets
+            onMomentumScrollEnd={(event) => this.scrollEnded(event)}
+          >
+            {calendarDates.map((date) => this.renderMonthView(moment(date), eventDatesMap, inActiveDatesMap))}
+          </ScrollView>
+          :
+          <View ref={calendar => this._calendar = calendar}>
+            {calendarDates.map((date) => this.renderMonthView(moment(date), eventDatesMap, inActiveDatesMap))}
+          </View>
+        }
+      </View>
+    );
+  }
+}
---
+++ components/styles.js
@@ -0,0 +1,159 @@
+import { Dimensions, StyleSheet } from 'react-native';
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+  calendarContainer: {
+    //backgroundColor: '#f7f7f7',
+  },
+  monthContainer: {
+    width: DEVICE_WIDTH,
+  },
+  calendarControls: {
+    flexDirection: 'row',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  controlLeftButton: {
+    alignItems : 'flex-end',
+    justifyContent:'center'
+  },
+  headerCenter:{
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  controlRightButton: {
+    alignItems : 'flex-start',
+    justifyContent: 'center'
+  },
+  controlButtonText: {
+    margin: 10,
+    fontSize: 15,
+  },
+  title: {
+    //textAlign: 'center',
+    fontSize: 30,
+    fontWeight: '500',
+    color: 'black',
+    margin: 10,
+  },
+  calendarHeading: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor:'#ddd'
+  },
+  dayHeading: {
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+    marginVertical: 5,
+    color: 'black'
+  },
+  weekendHeading: {
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+    marginVertical: 5,
+    color: '#cccccc',
+  },
+  weekRow: {
+    flexDirection: 'row',
+  },
+  dayButton: {
+    alignItems: 'center',
+    width: DEVICE_WIDTH / 7,
+    paddingVertical : 5
+  },
+  dayButtonEvent: {
+    alignItems: 'center',
+    width: DEVICE_WIDTH / 7,
+    backgroundColor: '#FFD90C',
+    paddingVertical : 5
+  },
+  dayButtonFiller: {
+    width: DEVICE_WIDTH / 7,
+  },
+  day: {
+    fontSize: 16,
+    alignSelf: 'center',
+    color:'black'
+  },
+  dayInActive:{
+    fontSize: 16,
+    alignSelf: 'center',
+    textDecorationLine : 'line-through',
+    color:'#999'
+  },
+  eventIndicatorFiller: {
+    marginTop: 3,
+    borderColor: 'transparent',
+    width: 4,
+    height: 4,
+    borderRadius: 2,
+  },
+  eventIndicator: {
+    backgroundColor: '#cccccc',
+  },
+  dayCircleFiller: {
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    width: 28,
+    height: 28,
+  },
+  currentDayCircle: {
+    backgroundColor: 'red',
+  },
+  currentDayText: {
+    color: 'red',
+  },
+  selectedDayCircle: {
+    backgroundColor: '#FFD90C',
+  },
+  hasEventCircle: {
+  },
+  hasEventDaySelectedCircle: {
+  },
+  hasEventText: {
+  },
+  selectedDayText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  weekendDayText: {
+    color: '#cccccc',
+  },
+  viewLoanDate:{
+    flexDirection:'row',
+    paddingVertical:10
+  },
+  viewLoanFromDate:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  viewLoanCenterDate:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  viewLoanToDate:{
+    flex:1,
+    justifyContent:'center',
+    alignItems: 'center'
+  },
+  txtLoanDate:{
+    fontSize:20,
+    fontWeight:'500',
+    color:'black'
+  },
+  icon:{
+    width:30,
+    height:30
+  },
+  iconSlash:{
+    width:50,
+    height:50
+  }
+});
+
+export default styles;
